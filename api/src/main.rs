e3f6eaa51bf49358adce43ae007e2b999a330634
arielherself <arielherself@duck.com>
2025-07-27 00:36:26 +0800

    feat: init

---
+++ api/src/main.rs
@@ -0,0 +1,190 @@
+use std::sync::LazyLock;
+use std::{collections::HashMap, convert::Infallible, fs::File, io::Read, sync::Arc};
+
+use qbit_rs::model::{AddTorrentArg, Credential, TorrentFile, TorrentSource};
+use qbit_rs::Qbit;
+use serde::{Serialize, Deserialize};
+use tokio::sync::Mutex;
+use warp::Filter;
+use toml::Table;
+
+#[derive(Serialize)]
+struct Torrent {
+    name: String,
+    progress: f64,
+    size: i64,
+    dlspeed: i64,
+    upspeed: i64,
+    eta: i64,
+    content_path: String,
+    hash: String,
+    status: i64,
+}
+
+#[derive(Serialize)]
+struct TorrentList {
+    torrents: Vec<Torrent>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ResponseEntry {
+    id: usize,
+    promotion_time_type: i64,
+    promotion_until: String,
+    leechers: usize,
+    seeders: usize,
+    name: String,
+    small_descr: String,
+    times_completed: usize,
+    size: usize,
+    added: String,
+    hr: usize,
+    info_hash: String,
+    downhash: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Response {
+    status: i64,
+    data: Vec<ResponseEntry>,
+}
+
+#[tokio::main]
+async fn main() {
+    static CONFIG: LazyLock<toml::map::Map<String, toml::Value>> = LazyLock::new(|| {
+        let mut file = File::open("config.toml").unwrap();
+        let mut content = String::new();
+        file.read_to_string(&mut content).unwrap();
+        content.parse::<Table>().unwrap()
+    });
+
+    let qb = Arc::new(Mutex::new({
+        let qb_url = CONFIG.get("qb-url").unwrap().as_str().unwrap();
+        let qb_username = CONFIG.get("qb-username").unwrap().as_str().unwrap();
+        let qb_password = CONFIG.get("qb-password").unwrap().as_str().unwrap();
+        let credential = Credential::new(qb_username, qb_password);
+        Qbit::new(qb_url, credential)
+    }));
+
+    let client = Arc::new(Mutex::new(reqwest::Client::new()));
+
+    let ping = warp::get().and(warp::path!("ping")).map(|| "pong!");
+
+    let search = warp::get()
+        .and(warp::path!("search"))
+        .and(warp::query::<HashMap<String, String>>())
+        .and_then({
+            let client = Arc::clone(&client);
+            move |query: HashMap<String, String>| {
+                let client = Arc::clone(&client);
+                async move {
+                    let keyword = query.get("keyword").unwrap().to_owned();
+
+                    let mut params = HashMap::new();
+                    params.insert("keyword", keyword);
+                    params.insert("page_size", "100".to_string());
+                    let response = client.lock().await
+                        .post("https://api.hddolby.com/api/v1/torrent/search")
+                        .header("x-api-key", CONFIG.get("passkey").unwrap().as_str().unwrap().to_string())
+                        .form(&params)
+                        .send().await.unwrap();
+
+                    let response_text = response.text().await.unwrap();
+
+                    let response_data = serde_json::from_str::<Response>(&response_text).unwrap();
+
+                    Ok::<String, Infallible>(serde_json::to_string(&response_data).unwrap())
+                }
+            }
+        });
+
+    let torrent_list = warp::get()
+        .and(warp::path!("torrent-list"))
+        .and_then({
+            let qb = Arc::clone(&qb);
+            move || {
+                let qb = Arc::clone(&qb);
+                async move {
+                    let qb_torrent_list = qb.lock().await.get_torrent_list(Default::default()).await.unwrap();
+                    let torrent_list = TorrentList {
+                        torrents: qb_torrent_list.into_iter().map(|tr| Torrent {
+                            name: tr.name.unwrap(),
+                            progress: tr.progress.unwrap(),
+                            size: tr.size.unwrap(),
+                            dlspeed: tr.dlspeed.unwrap(),
+                            upspeed: tr.upspeed.unwrap(),
+                            eta: tr.eta.unwrap(),
+                            content_path: tr.content_path.unwrap(),
+                            hash: tr.hash.unwrap(),
+                            status: match tr.state.unwrap() {
+                                qbit_rs::model::State::Uploading => 0,
+                                qbit_rs::model::State::Downloading => 1,
+                                _ => 2,
+                            }
+                        }).collect()
+                    };
+                    Ok::<String, Infallible>(serde_json::to_string(&torrent_list).unwrap())
+                }
+            }
+        });
+
+    let download = warp::get()
+        .and(warp::path!("download"))
+        .and(warp::query::<HashMap<String, String>>())
+        .and_then({
+            let client = Arc::clone(&client);
+            let qb = Arc::clone(&qb);
+            move |query: HashMap<String, String>| {
+                let client = Arc::clone(&client);
+                let qb = Arc::clone(&qb);
+                async move {
+                    let id = query.get("id").unwrap().to_owned();
+                    let downhash = query.get("downhash").unwrap().to_owned();
+                    let response = client.lock().await
+                        .get(format!("https://www.hddolby.com/download.php?id={id}&downhash={downhash}"))
+                        .header("User-Agent", "MediaArchive/0.1.0")
+                        .send().await.unwrap();
+
+                    let torrent_data = response.bytes().await.unwrap();
+                    let save_root = CONFIG.get("save-root").unwrap().as_str().unwrap().to_string();
+                    let save_path = format!("{save_root}/{}", id.clone());
+
+                    match qb.lock().await.add_torrent(&AddTorrentArg::builder()
+                        .source(TorrentSource::TorrentFiles {
+                            torrents: vec![TorrentFile {
+                                filename: id.clone(),
+                                data: torrent_data.to_vec()
+                            }]
+                        })
+                        .savepath(save_path)
+                        .root_folder("true".to_string())
+                        .sequential_download("true".to_string())
+                        .build()
+                    ).await {
+                        Ok(()) => Ok::<String, Infallible>(r#"{status: "success", message: ""}"#.to_string()),
+                        Err(e) => Ok(format!(r#"{{status: "error", message: "{e}"}}"#)),
+                    }
+                }
+            }
+        });
+
+    let stop_download = warp::get()
+        .and(warp::path!("stop"))
+        .and(warp::query::<HashMap<String, String>>())
+        .and_then({
+            let qb = Arc::clone(&qb);
+            move |query: HashMap<String, String>| {
+                let qb = Arc::clone(&qb);
+                async move {
+                    let hash = query.get("hash").unwrap().to_owned();
+                    match qb.lock().await.delete_torrents(vec![hash], true).await {
+                        Ok(()) => Ok::<String, Infallible>(r#"{status: "success", message: ""}"#.to_string()),
+                        Err(e) => Ok(format!(r#"{{status: "error", message: "{e}"}}"#)),
+                    }
+                }
+            }
+        });
+
+    warp::serve(ping.or(search).or(torrent_list).or(download).or(stop_download))
+        .run(([127, 0, 0, 1], 3000)).await;
+}
